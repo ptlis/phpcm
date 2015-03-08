41fbc3815c5b3926b66467d598f2ef6a3cd83216
ptlis <ptlis@ptlis.net>
2015-03-08 19:46:07 +0000

    Fix: Change clover coverage files to use placeholders.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,22 @@
 <?php
 
 require_once realpath(__DIR__ . '/../vendor/autoload.php');
+
+
+// Handle coverage paths
+$basePath = realpath(__DIR__ . '/..');
+
+$dataDirectory = $basePath . '/tests/data/';
+foreach (new DirectoryIterator($dataDirectory) as $directory) {
+    if (!$directory->isDot()) {
+
+        $baseCoveragePath = $dataDirectory . $directory . '/coverage.clover.base';
+        $realCoveragePath = $dataDirectory . $directory . '/coverage.clover';
+
+        if (file_exists($baseCoveragePath)) {
+            $coverageData = file_get_contents($baseCoveragePath);
+            $coverageData = str_replace('___PATH___', $basePath, $coverageData);
+            file_put_contents($realCoveragePath, $coverageData);
+        }
+    }
+}
